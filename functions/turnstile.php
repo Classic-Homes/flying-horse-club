67335fade6422a94b7724eeb816127189664db41
Chris Loidolt <loidolt@gmail.com>
2023-08-23 14:42:05 -0600

    Format changes

--- functions/turnstile.php
+++ functions/turnstile.php
@@ -5,20 +5,23 @@
  */
 
 // Add CF Turnstile JavaScript on login page
-function wpp_login_script() {
+function wpp_login_script()
+{
     wp_register_script('login-turnstile', 'https://challenges.cloudflare.com/turnstile/v0/api.js', false, NULL);
     wp_enqueue_script('login-turnstile');
 }
 add_action('login_enqueue_scripts', 'wpp_login_script');
 // Add CF Turnstile on login page
-function add_turnstile_on_login_page() {
+function add_turnstile_on_login_page()
+{
     echo '<div class="cf-turnstile" data-sitekey="0x4AAAAAAAJLDQ7pEtDRAPEe"></div>';
 }
-add_action('login_form','add_turnstile_on_login_page');
+add_action('login_form', 'add_turnstile_on_login_page');
 // Validating CF Turnstile on login page
-function turnstile_login_check($user, $password) {
-$postdata =$_POST['cf-turnstile-response'];
-   //add secret key
+function turnstile_login_check($user, $password)
+{
+    $postdata = $_POST['cf-turnstile-response'];
+    //add secret key
     $secret = '0x4AAAAAAAJLDZioIcmmsWdRbQyk0y0lwBM';
     $headers = array(
         'body' => [
@@ -29,17 +32,17 @@ $postdata =$_POST['cf-turnstile-response'];
     $verify = wp_remote_post('https://challenges.cloudflare.com/turnstile/v0/siteverify', $headers);
     $verify = wp_remote_retrieve_body($verify);
     $response = json_decode($verify);
-    if($response->success) {
+    if ($response->success) {
         $results['success'] = $response->success;
     } else {
         $results['success'] = false;
     }
-    if(empty($postdata)){ 
-        wp_die(__("<b>ERROR: </b><b>Please click the challenge checkbox.</b><p><a href='javascript:history.back()'>« Back</a></p>")); 
-    } elseif(!$results['success']){ 
-        wp_die(__("<b>Sorry, spam detected!</b>")); 
+    if (empty($postdata)) {
+        wp_die(__("<b>ERROR: </b><b>Please click the challenge checkbox.</b><p><a href='javascript:history.back()'>« Back</a></p>"));
+    } elseif (!$results['success']) {
+        wp_die(__("<b>Sorry, spam detected!</b>"));
     } else {
         return $user;
-    }   
+    }
 }
 add_action('wp_authenticate_user', 'turnstile_login_check', 10, 2);
